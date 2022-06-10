8c9a8beee6423dac9a668cba9ba17d2c9081cb37
Alex <xroker@gmail.com>
2022-06-10 15:44:38 -0600

    ts-jest: temporary disable type checking.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        // Temporary disable type checking during test runs.
+        // TODO: fix when the issue is resolved: https://github.com/kulshekhar/ts-jest/issues/943
+        exclude: ['**'],
+      },
+    },
+  }
 };
